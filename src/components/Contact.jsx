c05027d1189cd881ede8fbfb1894f2f5e809efa9
Zivgl66 <95753868+Zivgl66@users.noreply.github.com>
2023-06-24 19:46:56 +0300

    added icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { BsGithub } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
+import { AiOutlineLinkedin } from "react-icons/ai";
 
 const Contact = () => {
   return (
@@ -11,14 +14,17 @@ const Contact = () => {
             window.open("mailto:zivgl66@gmail.com?subject=Hello Ziv&body=Hi!")
           }
         >
+          <AiOutlineMail size={"2rem"} />
           Email
         </button>
         <button
           className="contact-button"
           onClick={() => window.open("https://github.com/Zivgl66", "_blank")}
         >
+          <BsGithub size={"2rem"} />
           GitHub
         </button>
+
         <button
           className="contact-button"
           onClick={() =>
@@ -28,6 +34,7 @@ const Contact = () => {
             )
           }
         >
+          <AiOutlineLinkedin size={"2rem"} />
           LinkedIn
         </button>
       </div>
